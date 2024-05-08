2f975ab09b8b5a3c7f708ddfdcd983d4b508082e
SteinsGate1097302 <2283372718@qq.com>
2024-05-09 01:03:47 +0800

    add 支持加载yml、yaml格式配置文件

--- rpc/src/main/java/com/ltl/rpc/server/VertxHttpServer.java
+++ rpc/src/main/java/com/ltl/rpc/server/VertxHttpServer.java
@@ -2,7 +2,6 @@ package com.ltl.rpc.server;
 
 import com.ltl.rpc.handler.HttpServerHandler;
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.http.HttpServerOptions;
 
 public class VertxHttpServer extends AbstractVerticle {
     private final Integer port;
@@ -18,7 +17,7 @@ public class VertxHttpServer extends AbstractVerticle {
 
             .listen(port, result -> {
                 if (result.succeeded()) {
-                    System.out.println("Server is now listening!");
+                    System.out.println("Server is now listening port: " + port + "!");
                 } else {
                     System.err.println("Failed to listen on port " + port);
                 }
--- rpc/src/main/java/com/ltl/rpc/util/ConfigUtil.java
+++ rpc/src/main/java/com/ltl/rpc/util/ConfigUtil.java
@@ -1,7 +1,13 @@
 package com.ltl.rpc.util;
 
+import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.setting.dialect.Props;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class ConfigUtil {
 
@@ -24,14 +30,39 @@ public class ConfigUtil {
      * @return
      * @param <T>
      */
-    public static <T> T loadConfig(Class<T> clazz, String prefix, String environment){
+    public static <T> T loadConfig(Class<T> clazz, String prefix, String environment) {
         StringBuilder configName = new StringBuilder("application");
-        if (StrUtil.isNotBlank(environment)){
+        if (StrUtil.isNotBlank(environment)) {
             configName.append("-").append(environment);
         }
-        configName.append(".properties");
-        Props props = new Props(configName.toString());
+
+        String[] fileFormats = {"properties", "yaml", "yml"};
+        Props props = null;
+        // 遍历读取支持的配置文件
+        for (String format : fileFormats) {
+            String fileName = configName + "." + format;
+            try (InputStream input = ConfigUtil.class.getClassLoader().getResourceAsStream(fileName)) {
+                if (input != null) {
+                    if ("properties".equalsIgnoreCase(format)) {
+                        props = new Props(fileName);
+                    } else if ("yaml".equalsIgnoreCase(format) || "yml".equalsIgnoreCase(format)) {
+                        Yaml yaml = new Yaml();
+                        props = yaml.loadAs(input, Props.class);    // yml配置文件处理
+                    }
+                    if (props != null) {
+                        break;
+                    }
+                }
+            } catch (IOException e) {
+                // 处理异常
+            }
+        }
+
+        if (props == null) {
+            throw new IllegalArgumentException("No supported configuration file found");
+        }
 
         return props.toBean(clazz, prefix);
     }
+
 }
